cd8a486cee587c675c20d84128777d7984ced8a5
Deo Duong <deo.duong@outlook.com>
2023-10-28 09:10:03 +0700

    add button

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
